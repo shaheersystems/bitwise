61b5e572cc2a78cb2fc37c87a2669e3fc9d76fd0
shaheer_init <105648279+shaheersystems@users.noreply.github.com>
2023-08-25 16:48:02 +0500

    chage

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,6 +9,7 @@ function Dashboard() {
         <div className='flex-1 bg-white'>
           <DashboardNav />
           <h1 className="font-mono font-light px-3 py-2 bg-gray-900 text-white">This is GitHub workspace testing....</h1>
+          <hr/>
         </div>
       </div>
     </div>
