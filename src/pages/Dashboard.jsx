f4940edfa164da7dc9f7ee919cb5dc162f76f6bb
shaheer_init <105648279+shaheersystems@users.noreply.github.com>
2023-08-25 16:52:43 +0500

    code

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,7 +8,13 @@ function Dashboard() {
         <Sidebar />
         <div className='flex-1 bg-white'>
           <DashboardNav />
-          <h1 className="font-mono font-light px-3 py-2 bg-gray-900 text-white">This is GitHub workspace testing....</h1>
+          <h1 className="font-mono font-light px-3 py-2 bg-gray-900 text-white">
+            <pre>
+            {`await fetch('https://api.vercel.app/blog', {
+  next: { revalidate: 10 }, // Seconds
+});`}
+            </pre>
+          </h1>
         </div>
       </div>
     </div>
